cbf526d1a5c63edb309e839b14a8ccd759b70aa5
Tewanh <estebanrtorres3@gmail.com>
2025-10-24 20:21:32 -0300

    Initialize Tkinter window in Codigo.py
    Fix syntax errors and set up a basic Tkinter window.

--- Codigo.py
+++ Codigo.py
@@ -1,1 +1,9 @@
+import tkinter as tk
 
+def ventana():
+  root - tk.Tk()
+  root.title("simulacion") #Crea una ventana llamada "simulacion"#
+
+  root.mainloop() #loop infinito a la espera de eventos a los cuales reaccionar. El programa debe terminar cuando se cierre la ventana.#
+if __name__ --"__ventana__":
+  ventana()
