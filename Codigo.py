fdf5e970bbce445c885f1534d408a5fb00b30baf
Tewanh <estebanrtorres3@gmail.com>
2025-10-28 07:57:21 -0300

    Update Codigo.py

--- Codigo.py
+++ Codigo.py
@@ -1,9 +1,8 @@
 import tkinter as tk
 
-def ventana():
-  root - tk.Tk()
-  root.title("simulacion") #Crea una ventana llamada "simulacion"#
+simulacion = tk.Tk() #Crea una ventana llamada "simulacion"#
+app.configure(background="black")
+tk.Wm.wm_title(simulacion, "Prueba")
 
-  root.mainloop() #loop infinito a la espera de eventos a los cuales reaccionar. El programa debe terminar cuando se cierre la ventana#
-if __name__ --"__ventana__":
-  ventana()
+simulacion.mainloop()
+
