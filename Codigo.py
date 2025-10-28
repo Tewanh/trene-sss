6721fa62e2f70e268db969620977267dfc0f8521
Tewanh <estebanrtorres3@gmail.com>
2025-10-28 07:59:12 -0300

    Refactor comments in Codigo.py for clarity

--- Codigo.py
+++ Codigo.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 
-simulacion = tk.Tk() #Crea una ventana llamada "simulacion"#
-app.configure(background="black")
-tk.Wm.wm_title(simulacion, "Prueba")
+simulacion = tk.Tk() #Crea una ventana#
+app.configure(background="black")#pantalla (color:negro)#
+tk.Wm.wm_title(simulacion, "Prueba")#pantalla(nombre:Prueba)#
 
 simulacion.mainloop()
 
