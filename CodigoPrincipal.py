9e49b6b3b1f792c468d515f72b5a141fc3d3931a
Tewanh <estebanrtorres3@gmail.com>
2025-11-06 21:09:51 -0300

    Delete CodigoPrincipal.py

--- CodigoPrincipal.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from tkinter import ttk, messagebox
-
-pantalla = tk.Tk() #Crea una ventana#
-pantalla.configure(background="black")#pantalla (color:negro)#
-tk.Wm.wm_title(simulacion, "Prueba")#pantalla(nombre:Prueba)#
-
-simulacion.mainloop()
-
