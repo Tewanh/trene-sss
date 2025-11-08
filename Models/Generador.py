45573cbef583e0c27d8ae572697a15c53a697d40
Tewanh <estebanrtorres3@gmail.com>
2025-11-07 23:08:01 -0300

    Create Generador class with initialization parameters

---
+++ Models/Generador.py
@@ -0,0 +1,18 @@
+class Generador:
+  def _init_(self, hora_inicio: dt.datetime, hora_final: dt.datetime):
+    def _init_ (
+      self,
+      poblaciuon: int,
+      seed=1234;
+      fecha_inicial: date.datetime = dt:datetime (2025,1,1),
+      hora_apertura: dt.time = date.time(7,0),
+      hora_cierre: dt.time = dt.time(20,0),
+    ):
+    self.poblacion = poblacion
+    self.seed = seed
+    self.rdm = random.Random(seed)
+    self.hora_apertura= hora_apertura
+    self.hora_cierre = hora_cierre
+    self.current_datettimer: dt.datetime = fecha_inicial
+      
+    self.rdm = random.Random(123)
