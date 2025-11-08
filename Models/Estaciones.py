6241833ba63e239029917786ea72a746c8fa3eda
Tewanh <estebanrtorres3@gmail.com>
2025-11-07 23:06:49 -0300

    Add Estacion class with initialization method
    Fix initialization method and add attributes for Estacion class.

---
+++ Models/Estaciones.py
@@ -0,0 +1,11 @@
+import Tkinter as tk
+import datetime as dt
+import random
+from abc import ABC, abstractmethod  # ABSTRACT
+from collections.abc import Callable
+from typing import Any
+
+class Estacion:
+  def _init_(self, nombre: str, hora:inicio: dt.datetime,hora_final: dt.datetime):
+    self.nombre = nombre
+    self.generador = Generador(hora_inicio, hora_final)
--- Models/Trenes.py
+++
@@ -1,8 +0,0 @@
-import Tkinter as tk
-import datetime as dt
-import random
-from abc import ABC, abstractmethod  # ABSTRACT
-from collections.abc import Callable
-from typing import Any
-
-
