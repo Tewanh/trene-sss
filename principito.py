fad34ba22a636acbd6b46a104ae03a5024b7e63d
Tewanh <estebanrtorres3@gmail.com>
2025-11-08 23:52:58 -0300

    Create principito.py

---
+++ principito.py
@@ -0,0 +1,9 @@
+# principito.py
+from ProgramaPrincipal import interfaz
+
+def tonoto():
+    # Esto llama a la funcion main() definida en interfaz.py
+    interfaz.main() 
+
+if __name__ == "__main__":
+    tonoto()
