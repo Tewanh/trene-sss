0c509a95f7159b2a1e2c546cf0d6561948f0a47d
Tewanh <estebanrtorres3@gmail.com>
2025-11-08 22:29:15 -0300

    Update Estaciones.py

--- Models/Estaciones.py
+++ Models/Estaciones.py
@@ -9,3 +9,4 @@ class Estacion:
   def __init__(self, nombre: str, hora:inicio: dt.datetime,hora_final: dt.datetime):
     self.nombre = nombre
     self.generador = Generador(hora_inicio, hora_final)
+    self.poblacion = self.poblacion * 0.2
