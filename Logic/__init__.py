55e596f8720ed96a6a89bc45595871750a79aeb4
vennusss <dalmasofia12@gmail.com>
2025-11-08 22:34:10 -0300

    Update __init__.py

--- Logic/__init__.py
+++ Logic/__init__.py
@@ -1,1 +1,4 @@
+from .EstadoDeSimulacion import EstadoSimulacion
+from .Guardado import guardar_datos, cargar_datos, crear_menu_archivo
 
+__all__ = [ "EstadoSimulacion", "guardar_datos", "cargar_datos", "crear_menu_archivo"]
