d99c22da2658db8a8acc682702301078e43d0c9b
Tewanh <estebanrtorres3@gmail.com>
2025-11-18 22:10:49 -0300

    Update Colores.py

--- Configuracion/Colores.py
+++ Configuracion/Colores.py
@@ -1,4 +1,10 @@
-import math
-import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
-from matplotlib.patches import Rectangle
+# Poner Colores en RGB#
+BLANCO = (255, 255, 255)
+NEGRO = (0, 0, 0)
+ROJO = (255, 0, 0)  
+VERDE = (0, 255, 0) 
+AZUL = (0, 0, 255)  
+AZUL_CLARO = (0, 191, 255)
+AMARILLO = (255, 255, 0)
+GRIS = (128, 128, 128)
+NARANJA = (255, 165, 0) 
