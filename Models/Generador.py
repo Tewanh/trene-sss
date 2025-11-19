3605ede4495a5cc8df1b44281c6473d1f4ef84a0
Tewanh <estebanrtorres3@gmail.com>
2025-11-18 22:12:25 -0300

    Update Generador.py

--- Models/Generador.py
+++ Models/Generador.py
@@ -1,29 +1,38 @@
-import Tkinter as tk
 import datetime as dt
 import random
 from abc import ABC, abstractmethod  # ABSTRACT
 from collections.abc import Callable
 from typing import Any
 
-class Generador:
-  Horas= dt.time(20,0).hour - dt.time(7,0).hour
-  Minutos= Horas * 60
-  print(Minutos)
-  CPM = self.poblacion * 0.2/Minutos
-  def _init_(self, hora_inicio: dt.datetime, hora_final: dt.datetime):
-    def _init_ (
-      self,
-      poblacion: int,
-      seed=1234,
-      fecha_inicial: date.datetime = dt:datetime (2025,1,1),
-      hora_apertura: dt.time = date.time(7,0),
-      hora_cierre: dt.time = dt.time(20,0),
+
+class Generador(ABC):
+    def __init__(
+        self,
+        poblacion: int,
+        seed: int = 123,
+        fecha_inicial: dt.datetime = dt.datetime(2025, 1, 1),
+        hora_apertura: dt.time = dt.time(7, 0),
+        hora_cierre: dt.time = dt.time(20, 0),
     ):
-    self.poblacion = poblacion
-    self.seed = seed
-    self.rdm = random.Random(seed)
-    self.hora_apertura= hora_apertura
-    self.hora_cierre = hora_cierre
-    self.current_datettimer: dt.datetime = fecha_inicial
-      
-    self.rdm = random.Random(123)
+        self.poblacion = poblacion
+        self.seed = seed
+        self.rdm = random.Random(seed)
+
+        self.hora_apertura = hora_apertura
+        self.hora_cierre = hora_cierre
+
+        self.current_datetime: dt.datetime = fecha_inicial
+
+    def minutos_de_funcionamiento(self) -> int:
+        horas = self.hora_cierre.hour - self.hora_apertura.hour
+        return horas * 60
+
+    @abstractmethod
+    def generar_clientes(
+        self,
+        minutos: int,
+        constructor: Callable[[int, dt.datetime], any],
+        update: bool = True,
+    ) -> list[Any]:
+        pass
+
